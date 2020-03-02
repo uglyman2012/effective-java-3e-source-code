febfe526e60d56420826207c29ca5f0c2a67c460
yang <123>
2020-03-02 16:46:26 +0800

    ceshi1

--- src/effectivejava/chapter2/item2/builder/NutritionFacts.java
+++ src/effectivejava/chapter2/item2/builder/NutritionFacts.java
@@ -49,6 +49,7 @@ public class NutritionFacts {
     }
 
     public static void main(String[] args) {
+        System.out.println("1111");
         NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
                 .calories(100).sodium(35).carbohydrate(27).build();
     }
